56206f2633e9caf53c701d6f913470b6de6f18e7
DaniHasReal <tempoemail1010@gmail.com>
2026-02-19 12:01:12 +0700

    Update mt6765.mk

--- mt6765.mk
+++ mt6765.mk
@@ -146,5 +146,5 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 31
 #$(call inherit-product, vendor/samsung/a05m/a05m-vendor.mk)
 
 # Inherit the sign keys
-$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
+#$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 
